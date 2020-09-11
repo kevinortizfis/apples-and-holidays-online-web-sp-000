aa3543a3457215619602bb00aa6361b1d972f30e
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-09-11 23:59:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -38,7 +38,13 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash.each do |k,v|
+    if k == :spring
+    v.each do |k,v|
+      v << "Balloons"
+    end
+  end
+  end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
