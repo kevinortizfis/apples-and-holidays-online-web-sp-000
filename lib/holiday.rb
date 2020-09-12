c080a7f4fa5dcb6f44c94a10b78aa40a71108682
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-09-12 00:47:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,7 +80,8 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 holiday_hash.each do |k,v|
-  v.detect("BBQ")
+  v.each do |k,v|
+    v.find("BBQ")
 end
 
 end
