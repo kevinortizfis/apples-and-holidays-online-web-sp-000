fbd4e725a22aa5a7528c26674e47af8d4e22a870
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-09-12 00:04:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -43,11 +43,6 @@ def add_supply_to_memorial_day(holiday_hash, supply)
     v.each do |k,v|
       v << "Grill"
     end
-    if v.include?("Grill")
-    v.each do |k,v|
-      v << "Table Cloth"
-    end
-  end
   end
   end
 end
