1b1314aebef1392a534b2d6c3be00fac1b565494
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-09-12 00:14:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,6 +53,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   columbus_day = {columbus_day: []}
   holiday_hash.each do |k,v|
     v.each do |k,v|
+      
       v << supply_array
     end
   end
