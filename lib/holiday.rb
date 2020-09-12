c9467c27659c14393ef65460ad51826933279b22
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-09-12 00:16:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,9 +50,7 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-  holiday_hash.each do |k,v|
     holiday_hash[season][holiday_name] = supply_array
-  end
   #holiday_hash[:fall][:columbus_day] = ["Flags", "Italian Food", "Parade Floats"]
   holiday_hash
 end
