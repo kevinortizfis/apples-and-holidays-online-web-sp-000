696ec8d8ea1b17355a5ebbb8298db264f4c9334e
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-09-12 00:22:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,13 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+holiday_hash.each do |k,v|
+  puts "#{k}:"
+  v.each do |k,v|
+    puts "  #{k}:"
 
+  end
+end
 end
 
 def all_holidays_with_bbq(holiday_hash)
