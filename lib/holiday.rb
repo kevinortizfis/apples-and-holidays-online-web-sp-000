6a28ba1d2e8b5016e188f9d248f7097ee1c37541
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-09-12 00:33:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |k,v|
   puts "#{k.capitalize}:"
   v.each do |k,v|
-    puts "  #{k.to_s.gsub(/_/," ").capitalize}: " + "#{v.join(", ")}"
+    puts "  #{k.to_s.gsub!(/_/," ").capitalize}: " + "#{v.join(", ")}"
   end
 end
 end
